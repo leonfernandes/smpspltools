587342e134671fb6f0d752af8c1e097ad6776542
LeonFernandes <leon.fernandes@columbia.edu>
2023-08-10 00:30:21 -0400

    crate

--- R/smpspl_list.R
+++ R/smpspl_list.R
@@ -5,9 +5,10 @@
 #' @param model_list list of models to be fit.
 #' @param fun a [smpspl][smpspl::smpspl-package] function to be applied.
 #' @param ... parameters other than "object" of `fun`.
+#' @inheritParams furrr::future_map
 #' @export
 smpspl_list <-
-    function(model_list, fun, ...) {
+    function(model_list, fun, .options = furrr::furrr_options(), ...) {
         model_id <- rlang::sym("model_id")
         dot_args <- list(...)
         p <- progressr::progressor(along = model_list)
@@ -16,9 +17,13 @@ smpspl_list <-
                 p()
                 do.call(fun, append(list(object = .x), dot_args))
             }
+        my_crt <-
+            carrier::crate(
+                ~ tibble::tibble(fun = my_fun(.x))
+            )
         ret <-
             model_list |>
-            furrr::future_map(~ tibble::tibble(fun = my_fun(.x)))
+            furrr::future_map(my_crt, .options = .options)
         ret <-
             ret |>
             purrr::list_rbind(names_to = "model_id") |>
